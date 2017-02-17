f2dcd85cf3b5cbfcf9383f172c96b21a333ed9df
Bruce <brucev@theedge.ca>
2017-02-16 22:31:38 -0700

    Files of stickyheader

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "65%");
 new RevealOnScroll($(".testimonial"), "50%");
+var stickyHeader = new StickyHeader();
